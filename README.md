87dac237efc233fd6f735265036dcafd68edb2b7
pjehan <pierre.jehan@gmail.com>
2025-04-17 19:24:02 +0200

    First commit

---
+++ README.md
@@ -0,0 +1,10 @@
+# TypeScript
+
+## Installation
+
+```bash
+npm install -g revealexpress
+git clone https://github.com/pjehan-teaching/typescript
+cd typescript
+revealexpress
+```
---
+++ slideshow.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+  name: 'TypeScript',
+  stylesheets: [],
+  javascripts: ['assets/js/script.js'],
+  revealjs: {
+    width: '100%',
+    height: '100%'
+  }
+};
